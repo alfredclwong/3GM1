2dabb61971d46415cae83291ad6d4ef395a8219f
unknown <alfred.cl.wong@gmail.com>
2019-05-20 12:13:16 +0100

    Finished adding comments. Fixed graph legend code (I think) and added primitive patient ID check.

--- MainWindow.py
+++ MainWindow.py
@@ -12,6 +12,8 @@ import time
 
 from MedicalArduino import MedicalArduino
 
+# The following lines of code were provided by the L2S2 employee:
+# BEGIN
 import asyncio
 from datetime import datetime
 import requests
@@ -40,11 +42,14 @@ LONG_TIME_EPOCH: datetime = datetime(1800, 1, 1)
 def get_api_key_header(api_key):
     return {'Authorization': f'X-API-KEY {api_key}'}
 
+
 def from_long_time(long_date_time: int) -> datetime:
     return LONG_TIME_EPOCH + timedelta(seconds=long_date_time)
 
+
 def to_long_time(date_time: datetime) -> int:
     return int((date_time - LONG_TIME_EPOCH).total_seconds())
+
 
 def print_status(resp) -> bool:
     if resp.status_code == 200: return False
@@ -53,22 +58,24 @@ def print_status(resp) -> bool:
 
     return True
 
+
 def record_search(patient_ID):
     filters = [
-            {'operator': 1, 'property': 'IdNumber', 'value': patient_ID}
-        ]
+        {'operator': 1, 'property': 'IdNumber', 'value': patient_ID}
+    ]
 
     resp = requests.post(f'{API_URI}/record/search',
-                        json={'filters': filters},
-                        headers=get_api_key_header(API_KEY),
-                        cert=_cert)
+                         json={'filters': filters},
+                         headers=get_api_key_header(API_KEY),
+                         cert=_cert)
 
     if print_status(resp): return
 
     response_json = resp.json()
 
-    #pprint(response_json)
+    # pprint(response_json)
     return response_json['RecordSearchResult']['records'][0]['id']
+
 
 def data_create(record_id, template_id, control_values: list):
     payload = {'record_id': record_id,
@@ -76,9 +83,9 @@ def data_create(record_id, template_id, control_values: list):
                'control_values': control_values}
 
     resp = requests.post(f'{API_URI}/data/create',
-                        json={'data': payload, 'ignore_conflicts': True},
-                        headers=get_api_key_header(_user_api_key),
-                        cert=_cert)
+                         json={'data': payload, 'ignore_conflicts': True},
+                         headers=get_api_key_header(_user_api_key),
+                         cert=_cert)
 
     if print_status(resp): return
 
@@ -90,6 +97,8 @@ def data_create(record_id, template_id, control_values: list):
 
     if status != 0:
         pprint(response_json['PlateDataCreateResult'], width=120)
+# END
+
 
 class MainWindow(QtGui.QMainWindow):
     def __init__(self, parent=None):
@@ -105,23 +114,24 @@ class MainWindow(QtGui.QMainWindow):
         super(MainWindow, self).__init__(parent)
         self.central = QtGui.QWidget()
         self.setCentralWidget(self.central)
-        
-        # Top row
+
+        # Top row (horizontal box layout)
         self.top_row = QHBoxLayout()
+        # Form layout is a nice way to contain multiple text input fields
         self.input_form = QFormLayout()
         self.id = QtGui.QLineEdit()
         self.input_form.addRow("Patient ID:", self.id)
         self.date = QtGui.QLineEdit()
         self.input_form.addRow("Date:", self.date)
         self.top_row.addLayout(self.input_form)
-        self.top_row.addWidget(QtGui.QPushButton("Settings"))
-        
-        # Graph
-        self.graph  = pg.PlotWidget()
-        
-        # Bottom row
+        self.top_row.addWidget(QtGui.QPushButton("Settings")) # dummy
+
+        # Middle row (just graph)
+        self.graph = pg.PlotWidget()
+
+        # Bottom row (horizontal box layout)
         self.bottom_row = QHBoxLayout()
-        self.dropdown = QtGui.QComboBox()
+        self.dropdown = QtGui.QComboBox() # todo: add selection functionality?
         self.bottom_row.addWidget(self.dropdown, stretch=1)
         self.refresh = QtGui.QPushButton("Detect")
         self.bottom_row.addWidget(self.refresh)
@@ -129,36 +139,31 @@ class MainWindow(QtGui.QMainWindow):
         self.bottom_row.addWidget(self.startstop)
         self.send = QtGui.QPushButton("Send")
         self.bottom_row.addWidget(self.send)
-        
-        # Add all 3 parts to self.layout
+
+        # Add all 3 rows to self.layout and assign self.layout to self.central
         self.layout = QGridLayout()
         self.layout.addLayout(self.top_row, 0, 0)
         self.layout.addWidget(self.graph, 1, 0)
         self.layout.addLayout(self.bottom_row, 2, 0)
         self.central.setLayout(self.layout)
-        
+
         # Now add functionality
         # Bottom row
-        
-        # Graph
-        self.timer = QtCore.QTimer()
-        #self.samplingrates = [2, 1]
-        #self.labels = ['S','H']
-        #self.prevs = [time.time()] * len(self.labels)
-        #self.ser = serial.Serial('COM3', 115200, timeout=0)
         self.refresh.clicked.connect(self.refresh_ports)
         self.startstop.clicked.connect(self.plotter)
         self.send.clicked.connect(self.sender)
-        
-        # Other stuff
+
+        # Other stuff - for keeping track of MedicalArduino instances and timing
         self.arduinos = []
+        
+        self.timer = QtCore.QTimer()
         self.prevs = []
-    
+
     def refresh_ports(self):
         """
         Within refresh_ports():
         1. Detect connection
-        2. Send header request
+        2. Send header request ('A')
         3. Create MedicalArduino() using header
         
         Outside refresh_ports():
@@ -169,86 +174,104 @@ class MainWindow(QtGui.QMainWindow):
         for arduino in self.arduinos:
             arduino.ser.close()
         
+        # Reset graph
+        self.graph.clear()
+        self.graph.getPlotItem().addLegend()
+        
+        # Udpate self.dropdown using serial.tools.list_ports.comports()
         self.dropdown.clear()
         for p in serial.tools.list_ports.comports():
             print("Detected device at port", p.device)
             ser = serial.Serial(p.device, 115200, timeout=0)
-            time.sleep(2)
+            time.sleep(2) # temporary workaround
             ser.write(b'A')
-            time.sleep(0.5)
-            while True:
+            time.sleep(0.5) # temporary workaround
+            while True: # read until endline ('\n') in case json is buffered
                 j = ser.readline()
                 if j.endswith(b'\n'):
                     break
             header = json.loads(j)
             print(header)
+
+            # Create a new MedicalArduino using this information
             arduino = MedicalArduino(ser, header, self.graph.getPlotItem())
             self.arduinos.append(arduino)
-            self.prevs.append(0)
+            self.prevs.append(0) # such that data will be requested on '>/=' click
             self.dropdown.addItems([arduino.name])
-    
-    def plotter(self): # called when the start/stop button is clicked
+
+    def plotter(self):  # called when the start/stop button is clicked
         """
         Set up graph according to selected arduinos and start timer
+        TODO: legends not showing at the moment
         """
         if not self.timer.isActive():
-            print("Recording started")
-            # Set up graph
-            #self.graph.getPlotItem().clear()
+            # Reset data plots
             for arduino in self.arduinos:
                 for label in arduino.data_labels:
                     arduino.data[label] = []
-            self.graph.getPlotItem().addLegend()
-            
+
             # Connect the updater() function to the clock and start it
             self.timer.timeout.connect(self.updater)
             self.timer.start(0)
+            print("Recording started")
         else:
-            print("Recording ended")
             self.timer.stop()
+            print("Recording ended")
 
-    def updater(self): # called regularly by QTimer
+    def updater(self):  # called regularly by QTimer
         """
-        Query arduinos for new data according to their sampling rates
-        Update graph according to arduino data
+        Query arduinos for new data according to their sampling rates.
+        Currently arduinos will also update the graph themselves.
         """
         for i in range(len(self.arduinos)):
             if time.time() - self.prevs[i] > 1.0 / self.arduinos[i].sampling_rate:
                 self.arduinos[i].sample()
                 self.prevs[i] = time.time()
-        
-    def sender(self): # called when the send button is clicked
+
+    def sender(self):  # called when the send button is clicked
+        """
+        1. Read patient ID from GUI text field
+        2. Extract data from MedicalArduino list
+        """
         if not self.timer.isActive():
             print("Sending data to Xenplate...")
-            
+
+            # Read patient ID
             print("Patient ID:", self.id.text())
             patient_ID = self.id.text()
-            
+            if patient_ID == "":
+                print("Error: no patient ID input!")
+                return
+
             # Check for no data
-            if not any(any(len(arduino.data[label])>0 for label in arduino.data_labels)
-                                                      for arduino in self.arduinos):
+            if not any(any(len(arduino.data[label]) > 0 for label in arduino.data_labels)
+                       for arduino in self.arduinos):
                 print("No data to send!")
                 return
             
+            # Extract data
             data = {}
             for arduino in self.arduinos:
                 for label in arduino.data_labels:
                     data[label] = np.mean(arduino.data[label])
             print(data)
-            
+
+            # Convert to uploadable format
             values = [{'name': 'Date', 'value': to_long_time(datetime.now())},
                       {'name': 'Time', 'value': to_long_time(datetime.now())},
                       {'name': 'Pulse', 'value': data['Heart_rate']},
                       {'name': 'Oxygen', 'value': data['Oxygen']}]
-            
+
+            # Look up Xenplate patient record using patient ID and create an entry
             data_create(record_search(patient_ID),
                         '484d0310-f2b7-4cbb-8258-215e3cb0ffc2',
-                        values)          
-            
+                        values)
+
             self.graph.getPlotItem().clear()
             print("Sent.")
         else:
             print("Recording still ongoing - end recording before sending data")
+
 
 if __name__ == '__main__':
     app = QtGui.QApplication([])
