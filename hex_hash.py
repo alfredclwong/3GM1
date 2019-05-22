5589d970df016e3ccf85a698be696bde3c91aa32
unknown <alfred.cl.wong@gmail.com>
2019-05-22 20:16:24 +0100

    Changed MD5 to SHA256, added nano.hex for demonstration

--- hex_hash.py
+++ hex_hash.py
@@ -8,15 +8,17 @@ avr_conf = os.path.join(avr_dir, "etc\\avrdude.conf")
 port = "COM3"
 hex_fname = "nano.hex"
 
-def md5(fname):
-    # Generate MD5 hash from the hex file
-    hash_md5 = hashlib.md5()
+def sha256(fname):
+    # Generate SHA256 hash from the hex file
+    hash_sha256 = hashlib.sha256()
     with open(hex_fname, "rb") as f:
         for chunk in iter(lambda: f.read(4096), b""):
-            hash_md5.update(chunk)
-        print(hash_md5.hexdigest())
+            hash_sha256.update(chunk)
+        print(hash_sha256.hexdigest())
 
 # Call avrdude.exe to extract and save the Arduino's hex file
 subprocess.check_call(f"\"{avr_exe}\" -c arduino -p m328p -C \"{avr_conf}\" -P \"{port}\" -b 57600 -U flash:r:{hex_fname}:i")
-md5(hex_fname)
+
+# Generate SHA256 checksum from the hex file
+sha256(hex_fname)
 
