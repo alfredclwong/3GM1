dd9203d52fbdddf751edb4c68f054ed3c933076a
Alfred Wong <alfred.cl.wong@gmail.com>
2019-05-27 12:50:31 +0100

    fleshed out MedicalData class

--- MedicalData.py
+++ MedicalData.py
@@ -1,7 +1,26 @@
-class MedicalData:
+class MedicalData(object):
     def __init__(self, label, active=True):
-        self.label = label
-        self.active = active
-        self.data = []
+        self._label = label
+        self._active = active
+        self._data = []
 
-    def 
+    @property
+    def label(self):
+        return label
+
+    @property
+    def active(self):
+        return active
+
+    @property
+    def data(self):
+        return data
+
+    def setActive(self, active):
+        self.active = active
+
+    def appendData(self, data):
+        self.data.append(data)
+
+    def clearData(self):
+        self.data = []
